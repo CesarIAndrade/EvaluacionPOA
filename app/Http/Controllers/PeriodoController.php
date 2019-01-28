3274e13ca7e203849115c11754206edfd1b5d2fd
CesarIAndrade <42975509+CesarIAndrade@users.noreply.github.com>
2019-01-28 18:20:27 -0500

    Avance doc.js

--- app/Http/Controllers/PeriodoController.php
+++ app/Http/Controllers/PeriodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Periodo;
 use Illuminate\Http\Request;
+use Response;
 
 class PeriodoController extends Controller
 {
--- public/js/GestionPeriodo.js
+++ public/js/GestionPeriodo.js
@@ -4,7 +4,20 @@ function crear_periodo(){
 
 function listar_periodo(){
     $('#tabla_periodo').html('');
-    
+    $.get("periodos",
+    function (data) {
+        $.each(data, function (index, val) { 
+             var periodo = '<tr id="periodo'+val.id+'">\
+             <td>'+val.id_telefono+'</td>\
+             <td>'+val.fecha_inicio+'</td>\
+             <td>'+val.fecha_fin+'</td>\
+             <td>'+val.estado+'</td>\
+             <td><button class="btn btn-info update_tlf" id="periodo'+val.id+'" value="'+val.id+'">Modificar</button></td>\
+             <td><button class="btn btn-danger delete_tlf" id="periodo'+val.id+'" value="'+val.id+'">Borrar</button></td></tr>'
+             $('#tabla_periodo').append(periodo);
+        });
+    },
+);
 
 }
 
