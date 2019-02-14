8a97160e241f8d79a8a327eb0026b3134c1f2bd5
CesarIAndrade <42975509+CesarIAndrade@users.noreply.github.com>
2019-02-14 10:48:18 -0500

    Subir Evidencias
    Cambio estético

--- resources/views/GestionEvidencias/cuerpo_modal_evidencias.blade.php
+++ resources/views/GestionEvidencias/cuerpo_modal_evidencias.blade.php
@@ -1,10 +1,4 @@
 <div class="row">
-    <div class="col-md-12">
-        <div class="form-group has-feedback border">
-            <label for="archivo_subido">{{ __('Subir Archivo PDF') }}</label>
-            <input autocomplete="off" id="archivo_subido" class="form-control-file" type="file" name="archivo_subido" accept=".pdf" required >
-        </div>
-    </div>
     <div class="col-md-6">
         <div class="form-group has-feedback">
             <label for="id_porcentaje_esperado">{{ __('Porcentaje esperado') }}</label>
@@ -18,3 +12,25 @@
         </div>
     </div>
 </div>
+
+<div class="form-div">
+    <label class ="btn btn-info" for="archivo_subido">
+        <span id = "nombre_archivo">Subir Archivo</span>
+    </label>
+    <input multiple = "true" style = "display:none" autocomplete="off" id="archivo_subido" class="form-control-file" type="file" name="archivo_subido" accept=".pdf" required >
+</div>
+
+<script>
+    $(document).ready(function(){
+        $('#archivo_subido').on('change', function(e){
+            var files = $(this)[0].files;
+            if(files.length >= 2){
+                $('#nombre_archivo').text(files.length + " archivos seleccionados")
+            }
+            else{
+                var file = e.target.value.split('\\').pop();
+                $('#nombre_archivo').text(file);
+            }
+        })
+    })
+</script>
